2926f9c986f2c2162e6379b190aa922fad79fe48
SamahAbdAlrahman <114294676+SamahAbdAlrahman@users.noreply.github.com>
2023-10-03 22:29:11 +0300

    Add files via upload

---
+++ common/color_extension.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class TColor {
+  static  Color get primary => const Color(0xc52cd5c7);
+
+  static Color get primaryText => const Color(0xff420b56);
+  //                            (66, 11, 86, 0.90)
+  static Color get secondaryText => const Color(0xff110000);
+  static Color get green=> const Color(0xffd639dc);
+  
+  static Color get white => Colors.white;
+  static Color get gray => const Color(0xff8C8C8C);
+  static Color get divider => const Color(0xffE1E1E1);
+
+}
